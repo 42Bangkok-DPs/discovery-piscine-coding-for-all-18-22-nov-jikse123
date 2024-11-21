6ecf8f7d0a1676ed33bb56ebf3afc9b4fe13a442
jikse123 <saunakomt11@gmail.com>
2024-11-21 20:25:39 +0000

    ddde

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,8 @@
+True:
+    a = int(input("Entr a number less than 25:"))
+    if a < 25:
+        for i in range(a,26):
+            print("Inside the loop, my variable is",i)
+            break
+        else:
+            print("Error")
