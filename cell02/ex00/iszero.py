fc20ccdb17e4f8c7875db8fa06a156a1b84c392f
jikse123 <saunakomt11@gmail.com>
2024-11-21 15:08:21 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,7 @@
+kum = int(input("Enter number: "))
+if kum < 0:
+    print("This number is negative.")
+elif kum == 0:
+    print("This number is zero.")
+else:
+    print("This number is positive.")
