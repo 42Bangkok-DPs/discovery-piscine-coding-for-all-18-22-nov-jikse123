ba2e1573bb64dfe04a6524a22f7c5db1a4bc5c09
jikse123 <saunakomt11@gmail.com>
2024-11-21 08:18:19 +0000

    dd

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+kum = int(input("Enter number: "))
+if kum < 0:
+    print("This number is negative.")
+elif kum == 0:
+    print("This number is both positive and negative.")
+else:
+    print("This number is positive.")
+
